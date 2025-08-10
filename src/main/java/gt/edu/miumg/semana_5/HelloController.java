56ad8b74a1bbd759c308405ae40a66397ad71660
SAJQUIYBOCH <bsajquiyb@miumg.edu.gt>
2025-08-09 23:17:14 -0600

    add

---
+++ src/main/java/gt/edu/miumg/semana_5/HelloController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gt.edu.miumg.semana_5;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author User
+ */
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+    @GetMapping
+    public String hello(){
+        return "App OK";
+    
+    }
+    
+}
